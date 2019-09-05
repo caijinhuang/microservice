cd8857ab8dd78b0136e04bdc9372faecd948891a
cjh <codecjh@163.com>
2019-09-05 11:22:26 +0800

    [新增][微服务学习][配置中心]配置中心git配置缓存目录变更

--- services/auth-server/src/main/java/com/cjh/study/authserver/AuthServerApplication.java
+++ services/auth-server/src/main/java/com/cjh/study/authserver/AuthServerApplication.java
@@ -2,11 +2,7 @@ package com.cjh.study.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
